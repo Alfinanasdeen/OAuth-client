45be9d46f41e48b0d5c83abdcbb09e5ab3a167f1
Alfinanasdeen <alfinanasdeen@gmail.com>
2024-12-12 09:01:34 +0530

    commit

--- src/pages/Signup/signup.jsx
+++ src/pages/Signup/signup.jsx
@@ -4,14 +4,14 @@ import styles from "./styles.module.css";
 function Signup() {
   const googleAuth = () => {
     window.open(
-      "https://oauth-server-l3vt.onrender.com/auth/google/callback",
+      "http://oauth-server-l3vt.onrender.com/auth/google/callback",
       "_self"
     );
   };
 
   const GithubAuth = () => {
     window.open(
-      "https://oauth-server-l3vt.onrender.com/auth/github/callback",
+      "http://oauth-server-l3vt.onrender.com/auth/github/callback",
       "_self"
     );
   };
